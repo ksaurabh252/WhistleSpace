966903bd3ff35311246a4a2d95b89ed46f66e5bd
ksaurabh252 <ksaurabh252@gmail.com>
2025-06-23 14:28:24 +0530

    feat: add protected admin route with AuthContext and PrivateRoute
    - Integrated AuthProvider to manage login state globally
    - Added route for /login and secured /admin using PrivateRoute
    - Updated routing structure to wrap routes inside AuthProvider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,26 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
 import SubmitPage from './pages/SubmitPage'
 import AdminFeedbackList from './pages/AdminFeedbackList'
+import LoginPage from './pages/LoginPage'
+import PrivateRoute from './components/PrivateRoute'
+import { AuthProvider } from './context/AuthContext'
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/submit" element={<SubmitPage />} />
-        <Route path="/admin" element={<AdminFeedbackList />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/submit" element={<SubmitPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/admin" element={
+            <PrivateRoute>
+              <AdminFeedbackList />
+            </PrivateRoute>
+
+          } />
+        </Routes>
+      </AuthProvider>
     </Router>
   )
 }
