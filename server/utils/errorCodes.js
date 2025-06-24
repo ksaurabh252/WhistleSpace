5f9e73b51fa9d45cf2f6163ba521c27db87d0420
ksaurabh252 <ksaurabh252@gmail.com>
2025-06-25 03:43:49 +0530

    feat(utils): add error codes module
    - Define common HTTP error codes (401, 403, 404, 400, 500)

---
+++ server/utils/errorCodes.js
@@ -0,0 +1,7 @@
+module.exports = {
+  UNAUTHORIZED: 401, // Missing or invalid authentication
+  FORBIDDEN: 403, // Authenticated but not allowed
+  NOT_FOUND: 404, // Resource doesn't exist
+  BAD_REQUEST: 400, // Invalid input/data
+  SERVER_ERROR: 500, // Unexpected server error
+};
