e89aaaa2d4055145dbafb6570efa22c71416de48
Saurabh Kumar <44322770+ksaurabh252@users.noreply.github.com>
2025-08-13 17:21:16 +0530

    beautify

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()], optimizeDeps: {
+  plugins: [react()],
+  optimizeDeps: {
     include: [
-      '@chakra-ui/react',
-      '@emotion/react',
-      '@emotion/styled',
-      'framer-motion',
+      "@chakra-ui/react",
+      "@emotion/react",
+      "@emotion/styled",
+      "framer-motion",
     ],
   },
-})
+});
